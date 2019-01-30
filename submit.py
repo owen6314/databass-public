72d57df0ce63cf75fe4705dc39f6124fd61f3a43
Haneen Mohammed <hamohammed.sa@gmail.com>
2019-01-30 01:16:25 -0500

    update submission script

--- submit.py
+++ submit.py
@@ -6,27 +6,21 @@ assignments = ['hw%s' % i for i in range(0,4)]
 digits = "1234567890"
 check_bad_uni = lambda uni: uni is None or any(c not in digits for c in uni[-4:])
 
-
-def validate_uni(ctx, param, uni):
-  if check_bad_uni(uni):
-    if param == "-u2" and uni == 'NONE': 
-      return
-    msg = "UNI should be in the format of AA1234.\nYou submitted: %s" % uni
-    raise click.BadParameter(msg)
-  return uni
-
 @click.command()
 @click.option("-u1", 
   prompt="Your UNI in the format of AA1234", 
-  help="UNI in AA1234 format",
-  callback=validate_uni)
+  help="UNI in AA1234 format")
+@click.option("-u2",
+  prompt="Your teammate's UNI or the word 'NONE' if you don't have a teammate",
+  help="Teammate's UNI, or 'NONE' if no teammate")
 @click.option('-a', prompt="The assignment you are submitting", type=click.Choice(assignments))
 @click.option('-q', is_flag=True, help="Submit without prompting")
-def main(u1, a, q):
+def main(u1, u2, a, q):
   """
   Script to package up your DataBass submission.  You should run this in a UNIX-based environment.
   """
   uni1 = u1
+  uni2 = u2
   assignment = a
 
   if check_bad_uni(uni1):
@@ -34,17 +28,25 @@ def main(u1, a, q):
     print("You submitted: %s" % uni1)
     return 
 
+  if check_bad_uni(uni2):
+    if uni2 == 'NONE':
+      pass
+    else:
+      print("Your teammate's UNI should be in the format of AA1234 or 'NONE' if you are working alone.")
+      print("You submitted: %s" % uni2)
+      return
+
   if assignment == None:
     print("Choose an assignment.  Use --help to see options")
     return
 
   if not q:
-    cmd = raw_input("You submitted %s and %s.  Type Y to submit: " % (uni1, assignment))
+    cmd = raw_input("You submitted %s, %s, and %s.  Type Y to submit: " % (uni1, uni2, assignment))
     if cmd.lower() != "y":
       return
 
   # Package and check the code
-  fname = "%s_%s" % (assignment, uni1)
+  fname = "%s_%s_%s" % (assignment, uni1, uni2)
   shutil.make_archive(fname, 'zip', "databass", ".")
 
 
